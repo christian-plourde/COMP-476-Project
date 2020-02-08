b629aea65df7b6c464e0e20a680db43a1b9fde5e
AyushKharade <ayush.kharade999@gmail.com>
2020-02-07 22:07:42 -0500

    added base files/folders

---
+++ COMP-476-Project/Assets/Scripts/Player/CameraBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Transform Target;
+
+    [Header("Offsets")]
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 Pos=transform.position;
+        Pos.x = Target.position.x+xOffset;
+        Pos.y = Target.position.y+yOffset;
+        Pos.z = Target.position.z+zOffset;
+
+        transform.position = Pos;
+
+        Vector3 LookAtPosition=Target.position;
+        LookAtPosition.y += 3;
+        transform.LookAt(LookAtPosition);
+    }
+}
---
+++ COMP-476-Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /*
+     * 8 Directional Movement with WASD 
+     * Will Align with movement direction before running (Align Behavior in this class)
+     */
+
+    [Header("Movement Parameters")]
+    float mSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
