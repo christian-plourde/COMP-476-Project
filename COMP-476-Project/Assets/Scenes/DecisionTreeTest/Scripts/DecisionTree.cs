11bedd93057e7c6198939289d04ea9dfdb6581e2
TheyCallMeJQ <jacques.a@libofthings.org>
2020-02-25 12:17:28 -0500

    Got decision tree working on spawned zombie; no actual movement, but he knows what he's supposed to do

---
+++ COMP-476-Project/Assets/Scenes/DecisionTreeTest/Scripts/DecisionTree.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class DTNode
+{
+    //The "current" node
+    public DTNode root;
+    //What to do if our condition evaluates to true (null on leaf)
+    public DTNode affirmative = null;
+    //What to do if our condition evaluates to false (null on leaf)
+    public DTNode negative = null;
+
+    public DTNode(DTNode root)
+    {
+        this.root = root;
+    }
+
+    //A function to be defined in ConditionNode and ActionNode
+    public virtual bool Evaluate()
+    {
+        //To be overridden
+        return false;
+    }
+
+    //Tells us whether the curent node is a leaf
+    public bool isLeaf()
+    {
+        return this.affirmative == null && this.negative == null;
+    }
+
+    //A node type for all non-leaf nodes; evaluates a condition
+    public class ConditionNode : DTNode
+    {
+        Func<bool> condition;
+
+        public ConditionNode(Func<bool> condition)
+            : base(null)
+        {
+            this.condition = condition;
+        }
+
+        public override bool Evaluate()
+        {
+            return this.condition();
+        }
+    }
+
+    //A node type for all leaf nodes; executes an action
+    public class ActionNode : DTNode
+    {
+        Action action;
+
+        public ActionNode(Action action)
+            : base(null)
+        {
+            this.action = action;
+        }
+
+        public override bool Evaluate()
+        {
+            this.action();
+            return false;
+        }
+    }
+}
+
+//A decision tree made up of DT nodes
+public class DecisionTree
+{
+    DTNode root;
+
+    //Build the tree by setting the root, which should have its children set beforehand
+    public DecisionTree(DTNode root)
+    {
+        this.root = root;
+    }
+
+    //Recursively evaluate all relevant conditions of the tree, based on the state we use to build the tree
+    public void Execute()
+    {
+        this.ExecuteNode(root);
+    }
+
+    //A function to execute one node of the decision tree
+    private void ExecuteNode(DTNode node)
+    {
+        //if the node is a leaf...
+        if (node.isLeaf())
+        {
+            //...then we have an action to execute
+            node.Evaluate();
+        }
+        //else if the node is not a leaf...
+        else
+        {
+            //...then evaluate the condition...
+            if (node.Evaluate())
+            {
+                //...if true, execute affirmative child
+                ExecuteNode(node.affirmative);
+            }
+            else
+            {
+                //...if false, execute negative child
+                ExecuteNode(node.negative);
+            }
+        }
+    }
+}
+
+
+
+////Usage example
+//public class Character
+//{
+//    public bool tired;
+//    public bool hungry;
+
+//    public bool IsHungry()
+//    {
+//        return this.hungry;
+//    }
+
+//    public bool IsTired()
+//    {
+//        return this.tired;
+//    }
+
+//    public void Sleep()
+//    {
+//        Debug.Log("Sleeping!");
+//    }
+
+//    public void Eat()
+//    {
+//        Debug.Log("Eating!");
+//    }
+
+//    public void EatAndSleep()
+//    {
+//        Debug.Log("Eating and sleeping!");
+//    }
+//}
+//
+//int main()
+//{
+//    Character character = new Character();
+//    //Set character state; based on this, the tree executes
+//    character.tired = false;
+//    character.hungry = true;
+
+//    DTNode.ConditionNode node = new DTNode.ConditionNode(character.IsTired);
+//    DTNode.ConditionNode n2 = new DTNode.ConditionNode(character.IsHungry);
+//    DTNode.ConditionNode n3 = new DTNode.ConditionNode(character.IsHungry);
+//    DTNode.ActionNode n4 = new DTNode.ActionNode(character.Sleep);
+//    DTNode.ActionNode n5 = new DTNode.ActionNode(character.Eat);
+//    DTNode.ActionNode n6 = new DTNode.ActionNode(character.Sleep);
+//    DTNode.ActionNode n7 = new DTNode.ActionNode(character.EatAndSleep);
+
+//    node.negative = n2;
+//    node.affirmative = n3;
+
+//    n2.negative = n4;
+//    n2.affirmative = n5;
+//    n3.negative = n6;
+//    n3.affirmative = n7;
+
+//    DecisionTree tree = new DecisionTree(node);
+//    tree.Execute();
+//}
---
+++ COMP-476-Project/Assets/Scenes/DecisionTreeTest/Scripts/ZombieMovement.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ZombieMovement : MonoBehaviour
+{
+    //Our zombie decision tree
+    public class ZombieDT
+    {
+        public Func<bool> seesTower;
+        public Func<bool> seesPlayer;
+        public Func<bool> towerNearby;
+        public Func<bool> playerNearby;
+
+        public Action attackTower;
+        public Action attackPlayer;
+        public Action moveToTower;
+        public Action moveToPlayer;
+        public Action wander;
+    }
+
+    //Our decision tree; manages whichever movement we should be doing
+    private DecisionTree m_DecisionTree = null;
+
+    //A placeholder tower variable. Later, this will likely need to be a list
+    private Transform m_Tower;
+    //A reference to the player variable, to be set on instantiation of the ZombieMovement
+    private Transform m_Player;
+
+    //What we consider to be nearby
+    public float m_WhatIsNearby;
+    //What our error margin should be, for what's considered to be in front of us
+    public float m_VisionErrorMargin;
+
+    //Our decision tree condition nodes
+
+    //For now I'll assume that if the player is somewhat in front of us, even if it's behind a wall, we can see it.
+    //Tells us whether the player is within sight of the zombie; to be used in the decision tree
+    private bool SeesPlayer()
+    {
+        Vector3 zombiepos = this.transform.position;
+        Vector3 playerpos = this.m_Player.position;
+        float dot = Vector3.Dot(this.transform.forward, (playerpos - zombiepos).normalized);
+        //If our dot product is exactly 1, then the player is exactly in front of us
+        float desired_result = 1.0f;
+        bool result = (desired_result - m_VisionErrorMargin <= dot && dot <= desired_result + m_VisionErrorMargin);
+        string message = (result) ? "Player spotted!" : "Player NOT spotted!";
+        Debug.Log(message);
+        //Debug.Log(message + " (" + dot + ")");//More specific logs
+        return (result);
+    }
+
+    //For now I'll assume that if the tower is somewhat in front of us, even if it's behind a wall, we can see it.
+    //Tells us whether a tower is within sight of the zombie; to be used in the decision tree
+    private bool SeesTower()
+    {
+        Vector3 zombiepos = this.transform.position;
+        Vector3 towerpos = this.m_Tower.position;
+        float dot = Vector3.Dot(this.transform.forward, (towerpos - zombiepos).normalized);
+        //If our dot product is exactly 1, then the tower is exactly in front of us
+        float desired_result = 1.0f;
+        bool result = (desired_result - m_VisionErrorMargin <= dot && dot <= desired_result + m_VisionErrorMargin);
+        string message = (result) ? "Tower spotted" : "Tower NOT spotted";
+        Debug.Log(message);
+        //Debug.Log(message + " (" + dot + ")");//More specific logs
+        return (result);
+    }
+
+    //Tells us whether a tower is within some distance of the zombie; to be used in the decision tree
+    private bool TowerNearby()
+    {
+        Vector3 zombiepos = this.transform.position;
+        Vector3 towerpos = this.m_Tower.position;
+        bool result = (zombiepos - towerpos).magnitude <= m_WhatIsNearby;
+        string message = (result) ? "Tower nearby!" : "Tower NOT nearby!";
+        Debug.Log(message);
+        //Debug.Log(message + "(" + (zombiepos - towerpos).magnitude + ")");//More specific logs
+        return (result);
+    }
+
+    //Tells us whether the player is within sight of the zombie; to be used in the decision tree
+    private bool PlayerNearby()
+    {
+        Vector3 zombiepos = this.transform.position;
+        Vector3 playerpos = this.m_Player.position;
+        bool result = (zombiepos - playerpos).magnitude <= m_WhatIsNearby;
+        string message = (result) ? "Tower nearby!" : "Tower NOT nearby!";
+        Debug.Log(message);
+        return (result);
+    }
+
+    //Our decision tree action nodes
+
+    private void AttackTower()
+    {
+        Debug.Log("Attacking tower!");
+    }
+    private void AttackPlayer()
+    {
+        Debug.Log("Attacking player!");
+    }
+    private void MoveToTower()
+    {
+        Debug.Log("Moving to tower!");
+    }
+    private void MoveToPlayer()
+    {
+        Debug.Log("Moving to player!");
+    }
+    private void Wander()
+    {
+        Debug.Log("Wandering!");
+    }
+
+    //A pseudoconstructor to allow us to easily spawn and initialize zombie movement types
+    public void Initialize(Transform tower, Transform player)
+    {
+        //Set our instance variables
+        this.m_Player = player;
+        this.m_Tower = tower;
+
+        //Set our decision tree
+        //Conditions
+        DTNode.ConditionNode r1n1 = new DTNode.ConditionNode(SeesTower);
+        DTNode.ConditionNode r2n1 = new DTNode.ConditionNode(TowerNearby);
+        DTNode.ConditionNode r2n2 = new DTNode.ConditionNode(SeesPlayer);
+        DTNode.ConditionNode r3n3 = new DTNode.ConditionNode(PlayerNearby);
+
+        //Actions
+        DTNode.ActionNode r3n1 = new DTNode.ActionNode(AttackTower);
+        DTNode.ActionNode r3n2 = new DTNode.ActionNode(MoveToTower);
+        DTNode.ActionNode r3n4 = new DTNode.ActionNode(Wander);
+        DTNode.ActionNode r4n1 = new DTNode.ActionNode(AttackPlayer);
+        DTNode.ActionNode r4n2 = new DTNode.ActionNode(MoveToPlayer);
+
+        //Assign the order of the tree, per row in our tree diagram (https://docs.google.com/drawings/d/1qOOZjceQnmuGH2RxBY521rWqPFpAjUt2HtGHsXuAP04/edit)
+        //Row 1
+        r1n1.affirmative = r2n1;
+        r1n1.negative = r2n2;
+        //Row 2
+        r2n1.affirmative = r3n1;
+        r2n1.negative = r3n2;
+        r2n2.affirmative = r3n3;
+        r2n2.negative = r3n4;
+        //Row 3
+        r3n3.affirmative = r4n1;
+        r3n3.negative = r4n2;
+
+        this.m_DecisionTree = new DecisionTree(r1n1);
+        //Debug.Log("Decision tree built!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.m_DecisionTree != null)
+        {
+            //Debug.Log("Executing decision tree!");
+            this.m_DecisionTree.Execute();
+        }
+    }
+}
