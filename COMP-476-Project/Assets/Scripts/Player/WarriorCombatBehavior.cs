1f76857b26d879a84f907339000b00fb1043a2af
AyushKharade <ayush.kharade999@gmail.com>
2020-02-25 09:52:18 -0500

    pushed warrior init setup

---
+++ COMP-476-Project/Assets/Scripts/Player/WarriorCombatBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorCombatBehavior : MonoBehaviour
+{
+
+
+    private Animator animator;
+    public bool Attacking; // if in attack mode, if false, you can build
+
+    //public GameObject ProjectilePrefab;
+    //public Transform LaunchPoint;
+    //public Transform AttackTarget;
+
+    Transform PlayerMesh;
+
+    [Header("Weapon Slots")]
+    public GameObject BackSword;
+    public GameObject HandSword;
+    //public GameObject HeldArrow;
+    //public GameObject AbilityCircle;
+
+    //float mouseClickTime = 0f;
+
+    PlayerMovement PlayerMovementRef;
+
+
+
+    [HideInInspector] public bool ultimateCooldown;
+    float ultimateCooldownTimer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.GetChild(0).GetComponent<Animator>();
+        PlayerMesh = transform.GetChild(0);
+
+        HandSword.SetActive(false);
+        BackSword.SetActive(false);
+
+        PlayerMovementRef = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
