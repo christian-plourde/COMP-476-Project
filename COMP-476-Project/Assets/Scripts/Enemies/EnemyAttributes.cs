b48b32f4fc44bc77e9c9a6dd8bc6ab0e23cb6bac
Val <valp.perrot@gmail.com>
2020-02-25 19:33:27 -0500

    #10: First tower and its projectile. Some new getters, fields, and bugfixes on EnemyAttributes.

--- COMP-476-Project/Assets/Scripts/Enemies/EnemyAttributes.cs
+++ COMP-476-Project/Assets/Scripts/Enemies/EnemyAttributes.cs
@@ -10,6 +10,7 @@ public class EnemyAttributes : MonoBehaviour
     public int damage;
     public float speed;
     public bool isDead;
+    public float heightOffset;
 
     Animator animator;
 
@@ -18,12 +19,25 @@ public class EnemyAttributes : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {       
         maxHealth = health;
         animator = GetComponent<Animator>();
     }
 
+    public float GetHeightOffset()
+    {
+        return heightOffset;
+    }
 
+    public bool GetIsDead()
+    {
+        return isDead;
+    }
+
+    public int GetHealth()
+    {
+        return health;
+    }
 
     public void DealDamage(int dmg)
     {
@@ -42,17 +56,18 @@ public class EnemyAttributes : MonoBehaviour
         animator.SetBool("Dead", true);
         isDead = true;
         GameObject playerRef = GameObject.FindGameObjectWithTag("Player");
-        if (playerRef.GetComponent<CombatBehavior>().AttackTarget!=null 
+        if (playerRef) {
+            if (playerRef.GetComponent<CombatBehavior>().AttackTarget != null
             &&
             playerRef.GetComponent<CombatBehavior>().AttackTarget.name == transform.name)
-        {
-            playerRef.GetComponent<CombatBehavior>().AttackTarget = null;
-            playerRef.GetComponent<CombatBehavior>().AcquireTarget();
-        }
+            {
+                playerRef.GetComponent<CombatBehavior>().AttackTarget = null;
+                playerRef.GetComponent<CombatBehavior>().AcquireTarget();
+            } }
         transform.GetChild(0).gameObject.SetActive(false);
 
         // remove UI Health display & destroy object in few seconds, remove own collider
-        GetComponent<SphereCollider>().enabled = false;
+        GetComponent<CapsuleCollider>().enabled = false;
         GetComponent<Rigidbody>().isKinematic = true;
         HealthUI.enabled = false;
         Destroy(this.gameObject, 60f);
