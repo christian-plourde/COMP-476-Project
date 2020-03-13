623a2954e9e2989e965268f8b3496a2aa164c691
AyushKharade <ayush.kharade999@gmail.com>
2020-03-13 13:31:30 -0400

    very cool main menu

---
+++ COMP-476-Project/Assets/Scripts/Misc/MenuWolf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWolf : MonoBehaviour
+{
+    float timer;
+
+    void Start()
+    {
+        GetComponent<Animator>().SetBool("Chasing",true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 9)
+            Destroy(this.gameObject);
+        transform.Translate(transform.forward*11f*Time.deltaTime);   
+
+    }
+}
---
+++ COMP-476-Project/Assets/Scripts/Misc/MenuWolfSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWolfSpawner : MonoBehaviour
+{
+    public float timer;
+    float spawnTimer;
+    bool spawning;
+
+    public GameObject wolfPrefab;
+    public Transform spawnLocation;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 85 && !spawning)
+        {
+            spawning = true;
+        }
+
+        if (spawning)
+        {
+            SpawnWolves();
+        }
+
+        void SpawnWolves()
+        {
+            spawnTimer += Time.deltaTime;
+            if (spawnTimer > 0.75f && timer<140)
+            {
+                spawnTimer = 0;
+                Vector3 spawnLoc = spawnLocation.position;
+                float offsetX = Random.Range(-1.5f,1.5f);
+                spawnLoc.x += offsetX;
+                Instantiate(wolfPrefab, spawnLoc, Quaternion.identity);
+            }
+        }
+    }
+}
