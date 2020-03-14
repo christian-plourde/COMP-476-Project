71b4cc6ba74f462a1cdb4576a4122391e29cbecb
AyushKharade <ayush.kharade999@gmail.com>
2020-03-14 14:45:49 -0400

    added troll in menu raid

--- COMP-476-Project/Assets/Scripts/Misc/MainMenu/MenuWolfSpawner.cs
+++ COMP-476-Project/Assets/Scripts/Misc/MainMenu/MenuWolfSpawner.cs
@@ -7,6 +7,7 @@ public class MenuWolfSpawner : MonoBehaviour
     public float timer;
     float spawnTimer;
     bool spawning;
+    bool spawnedTroll;
     bool finishedIntro;
 
 
@@ -15,6 +16,7 @@ public class MenuWolfSpawner : MonoBehaviour
     public GameObject wolfPrefab;
     public GameObject zombiePrefab;
     public GameObject halblitzerPrefab;
+    public GameObject trollPrefab;
     public Transform spawnLocation;
 
     
@@ -23,11 +25,15 @@ public class MenuWolfSpawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        
 
-        if (timer > 85 && !spawning && !finishedIntro)
+
+        if (timer > 82 && !spawning && !finishedIntro)
         {
             spawning = true;
+        }
+        else
+        {
+            spawning = false;
         }
 
         if (spawning)
@@ -38,20 +44,25 @@ public class MenuWolfSpawner : MonoBehaviour
         void SpawnWolves()
         {
             spawnTimer += Time.deltaTime;
-            if (spawnTimer > spawnFrequency && timer<105)
+            if (spawnTimer > spawnFrequency && timer < 105)
             {
                 spawnTimer = 0;
                 Vector3 spawnLoc = spawnLocation.position;
-                float offsetX = Random.Range(-1.5f,1.5f);
+                float offsetX = Random.Range(-1.5f, 1.5f);
                 spawnLoc.x += offsetX;
 
                 int r = Random.Range(0, 10);
-                if(r<=1)
+                if (r <= 1)
                     Instantiate(halblitzerPrefab, spawnLoc, Quaternion.identity);
-                else if(r<=5)
+                else if (r <= 5)
                     Instantiate(zombiePrefab, spawnLoc, Quaternion.identity);
                 else
                     Instantiate(wolfPrefab, spawnLoc, Quaternion.identity);
+            }
+            else if (timer > 100 && !spawnedTroll)
+            {
+                Instantiate(trollPrefab, spawnLocation.position, Quaternion.identity);
+                spawnedTroll = true;
             }
 
             if (timer > 120)
