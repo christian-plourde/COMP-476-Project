7e149c7c6f88c709203aafd8796cb6e0f31f05bb
AyushKharade <ayush.kharade999@gmail.com>
2020-03-18 11:52:16 -0400

    created base UI

--- COMP-476-Project/Assets/Scripts/Misc/BaseIntegrity.cs
+++ COMP-476-Project/Assets/Scripts/Misc/BaseIntegrity.cs
@@ -5,25 +5,62 @@ using UnityEngine.UI;
 
 public class BaseIntegrity : MonoBehaviour
 {
+    // if base health falls zero or below, you lose the game.
+    // base can be repaired (expensive) during prep phase only.
+
     public int BaseHealth;
+    int maxBaseHealth;
     bool defeated;
 
+    [Header("UI References")]
+    public Image healthBar;
+    public Text integrity;
+    public Text healthCount;
 
-    void Start()
+    private void Start()
     {
-        
+        maxBaseHealth = BaseHealth;
+        healthCount.text = maxBaseHealth+"";
     }
 
-    void Update()
+    void UpdateUI()
     {
-        
+        float healthPercentage = ((float)(BaseHealth) / (float)(maxBaseHealth));
+        healthBar.fillAmount = healthPercentage;
+        healthCount.text = BaseHealth+"";
+
+        // integrity
+        string str="";
+        if (healthPercentage > 0.9)
+            str = "Strong";
+        else if (healthPercentage > 0.7)
+            str = "Great";
+        else if (healthPercentage > 0.5)
+            str = "Holding";
+        else if (healthPercentage > 0.3)
+            str = "Weak";
+        else if (healthPercentage > 0.1)
+            str = "Critical!!";
+        else
+            str = "Failing!!!";
+             
+
+
+            integrity.text = "Base Integrity: "+str;
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Enemy")
         {
-
+            BaseHealth -= other.GetComponent<EnemyAttributes>().damageToBase;
+            UpdateUI();
+            if (BaseHealth < 0)
+            {
+                BaseHealth = 0;
+                // Call defeat Condition
+            }
         }
     }
 }
