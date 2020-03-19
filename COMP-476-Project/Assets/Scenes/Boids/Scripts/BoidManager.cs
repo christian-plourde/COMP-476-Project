843a1068cf339bc8ae0922635a036bab84508217
TheyCallMeJQ <jacques.a@libofthings.org>
2020-03-19 18:39:56 -0400

    Added basic boid movement

--- COMP-476-Project/Assets/Scenes/Boids/Scripts/BoidManager.cs
+++ COMP-476-Project/Assets/Scenes/Boids/Scripts/BoidManager.cs
@@ -16,6 +16,10 @@ public class BoidManager : MonoBehaviour
 
     List<Boid> m_Boids = new List<Boid>();
 
+    public static float DELTA_T = 0.025f;
+
+    public static float PADDING = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,22 +31,41 @@ public class BoidManager : MonoBehaviour
             float angle = i * (360.0f / m_NumBoids) * conversion;
             Vector3 spawn_pos = this.m_Anchor.position + new Vector3(spawn_radius * Mathf.Cos(angle), spawn_radius * Mathf.Sin(angle));
             this.m_Boids.Add(GameObject.Instantiate(this.m_BoidObject, spawn_pos, new Quaternion(), this.m_BoidContainer.transform).GetComponent<Boid>());
-            this.m_Boids[i].SetAnchor(this.m_Anchor);
+            this.m_Boids[i].Initialize(this.m_Anchor);
+            
         }
 
     }
 
-    void MoveBoids()
+    void UpdateBoids()
     {
-        foreach (Boid b in this.m_Boids)
+        for(int i = 0; i < this.m_Boids.Count; i++)
         {
+            //Get the position and velocity of all but the currently executing boid
+            List<Vector3> other_positions = new List<Vector3>();
+            List<Vector3> other_velocities = new List<Vector3>();
 
+            for (int j = 0; j < this.m_Boids.Count; j++)
+            {
+                if (i != j)
+                {
+                    other_positions.Add(this.m_Boids[j].Position);
+                    other_velocities.Add(this.m_Boids[i].Velocity);
+                }
+            }
+            //We have everybody else's position and velocity
+            Boid b = this.m_Boids[i];
+            b.ComputeR1(other_positions);
+            b.ComputeR2(other_velocities);
+            b.ComputeR3(other_positions);
+            b.ComputeR4();
+            b.ComputeInputVector();
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.UpdateBoids();
     }
 }
