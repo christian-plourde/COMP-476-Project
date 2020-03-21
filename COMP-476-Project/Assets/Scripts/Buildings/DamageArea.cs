eeaba6a1d6bec1405df8e33c227763823d7e1778
Val <valp.perrot@gmail.com>
2020-03-20 22:37:12 -0400

    #10: Added Pulse tower and its upgrade.

---
+++ COMP-476-Project/Assets/Scripts/Buildings/DamageArea.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageArea : MonoBehaviour
+{
+    public int cooldown;
+    public float damage, range;
+
+    private int scanTimer, cooldownTimer;
+
+    void Start()
+    {
+        cooldown *= 60;
+    }
+
+    void Update()
+    {
+        scanTimer++;
+        if (cooldownTimer < cooldown)
+        {
+            cooldownTimer++;
+        }
+        else
+        {
+            if (scanTimer >= 20)
+            {
+                scanTimer = 0;
+                Pulse();
+            }
+        }       
+    }
+
+    void Pulse()
+    {
+        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, range);
+        foreach (Collider col in collidersInRange)
+        {
+            if (col && validTarget(col.gameObject) == true)
+            {
+                // TODO add animation
+                col.gameObject.GetComponent<EnemyAttributes>().DealDamage(damage);
+                cooldownTimer = 0;
+            }
+        }
+    }
+
+    bool validTarget(GameObject potentialTarget)
+    {
+        if (potentialTarget.tag == "Enemy" &&
+            potentialTarget.GetComponent<EnemyAttributes>().GetIsDead() == false &&
+            (potentialTarget.transform.position - transform.position).magnitude < range)
+        {
+            return true;
+        }
+        return false;
+    }
+}
